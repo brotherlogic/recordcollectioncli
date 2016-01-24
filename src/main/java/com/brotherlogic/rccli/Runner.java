65260e65668c8d973142fad7584dfd03833d0bbf
Simon Tucker <brotherlogic@gmail.com>
2016-01-23 21:23:27 -0800

    Cleaned for google style

--- src/main/java/com/brotherlogic/rccli/Runner.java
+++ src/main/java/com/brotherlogic/rccli/Runner.java
@@ -2,8 +2,8 @@ package com.brotherlogic.rccli;
 
 public class Runner {
 
-	public static void main(String[] args){
-		
-	}
-	
+  public static void main(String[] args) {
+    System.out.println("Running Record Collection");
+  }
+
 }
